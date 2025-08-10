208649d7ad0bd5d96d3c4d8088790bd2e13e1ef4
Jullyo1122 <jullyoc74@gmail.com>
2025-08-09 22:09:28 -0300

    init: iniciando projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { DashboardComponent } from "./dashboard/dashboard.component";
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatGridListModule, DashboardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'dashboard-angular';
 }
+
